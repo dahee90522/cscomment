b28e056b8643047143573ad09575cf161904c704
dahee90522 <s2018s40@e-mirim.hs.kr>
2020-05-06 16:42:14 +0900

    초기 커밋.

---
+++ cscomment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cscomment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            Console.Write("x 입력: ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.Write("y 입력: ");
+            y = int.Parse(Console.ReadLine());
+
+            if (x > 4)
+            {
+                if (y > 2)
+                {
+                    Console.WriteLine(x * y);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
